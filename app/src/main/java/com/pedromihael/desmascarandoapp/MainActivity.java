d6f653180270d28eb3ef909a7d9a721aa9bbc81b
Hi-im-Yan <yanajiki2010@hotmail.com>
2020-06-27 15:22:23 -0400

    Trying to get photo from gallery

--- app/src/main/java/com/pedromihael/desmascarandoapp/MainActivity.java
+++ app/src/main/java/com/pedromihael/desmascarandoapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.pedromihael.desmascarandoapp;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,6 +14,7 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +26,12 @@ import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity implements NewCellphoneDialog.DialogListener {
 
     private TabLayout mTabLayout;
@@ -30,8 +40,8 @@ public class MainActivity extends AppCompatActivity implements NewCellphoneDialo
 
     //Photo Variables
     private final int CAPTURE_PHOTO = 100;
-    Uri uri;
-    ImageView img;
+    private Uri uri;
+    private String imgPath;
     //End Photos
 
     @Override
@@ -60,24 +70,53 @@ public class MainActivity extends AppCompatActivity implements NewCellphoneDialo
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 100);
         }
 
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(view -> {
-            Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//                String root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString() + "propic.png";
-//                uri = Uri.parse(root);
-            startActivityForResult(i, CAPTURE_PHOTO);
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+//            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(image));
+
+            startActivityForResult(intent, CAPTURE_PHOTO);
         });
 
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 100) {
-            Bitmap capImage = (Bitmap) data.getExtras().get("data");
-//            img.setImageBitmap(capImage);
+        if (requestCode == CAPTURE_PHOTO) {
+
+            uri = data.getData();
+            String[] filepath = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(uri, filepath, null,
+                    null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filepath[0]);
+            String filePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            //Toast.makeText(this, filePath, Toast.LENGTH_SHORT).show();
+
+            try {
+                getPhotoDetails(filePath);
+            } catch (IOException e) {
+                Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+            }
         }
+        else if (resultCode == RESULT_CANCELED) {
+            Toast.makeText(this, "Picture was not taken", Toast.LENGTH_SHORT);
+        }
+    }
+
+    private void getPhotoDetails(String filepath) throws IOException {
+        ExifInterface exif = new ExifInterface(filepath);
+        String teste = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
+//        String teste2 = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
+//        String teste3 = exif.getAttribute(ExifInterface.TAG_GPS_TIMESTAMP);
+
+        Toast.makeText(this, teste, Toast.LENGTH_SHORT).show();
     }
 
     private void openNewCellphoneDialog() {
