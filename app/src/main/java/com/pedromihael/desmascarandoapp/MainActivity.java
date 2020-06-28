05dda7cbdaf0acc364f91456c2a9bdc6637064d8
Hi-im-Yan <yanajiki2010@hotmail.com>
2020-06-28 19:19:02 -0400

    Now can shoot photos and save in gallery

--- app/src/main/java/com/pedromihael/desmascarandoapp/MainActivity.java
+++ app/src/main/java/com/pedromihael/desmascarandoapp/MainActivity.java
@@ -1,17 +1,22 @@
 package com.pedromihael.desmascarandoapp;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.location.Location;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -30,13 +35,19 @@ public class MainActivity extends AppCompatActivity implements PostDialog.Dialog
     private ViewPager mViewPager;
 
     //Photo Variables
+    private static final int PERMISSION_REQUEST_CODE = 200;
     private final int CAPTURE_PHOTO = 100;
     private Uri uri;
     private String imgPath;
     //End Photos
 
+    //Location Variables
+    //private FusedLocationProviderClient fusedLocationClient;
+    //End Location
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -63,11 +74,90 @@ public class MainActivity extends AppCompatActivity implements PostDialog.Dialog
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(view -> {
-            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            intent.setType("image/*");
+            /* Choosefrom gallery */
+//            Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//            intent.setType("image/*");
+//            startActivityForResult(intent, CAPTURE_PHOTO);
+            /**/
+
+            Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
             startActivityForResult(intent, CAPTURE_PHOTO);
+
         });
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPermission()) {
+            // Success
+        } else {
+            requestPermission();
+        }
+    }
+
+    private boolean checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            return false;
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            return false;
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            return false;
+        }
+        return true;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE},
+                PERMISSION_REQUEST_CODE);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission Granted",
+                            Toast.LENGTH_SHORT).show();
+                    // main logic
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!=
+                                PackageManager.PERMISSION_GRANTED) {
+                            showMessageOKCancel("You need to allow access permissions",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermission();
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
     }
 
 
@@ -75,38 +165,42 @@ public class MainActivity extends AppCompatActivity implements PostDialog.Dialog
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAPTURE_PHOTO) {
-            uri = data.getData();
-            openNewCellphoneDialog(uri);
 
-            String[] projection = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(uri, projection, null, null,null);
+            /*Choose from gallery*/
+//            uri = data.getData();
+//            String[] projection = { MediaStore.Images.Media.DATA };
+//            Cursor cursor = getContentResolver().query(uri, projection, null, null,null);
+//            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+//            cursor.moveToFirst();
+//            String path = cursor.getString(column_index);
 
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
 
-            String path = cursor.getString(column_index);
-
-            Toast.makeText(this, path, Toast.LENGTH_SHORT).show();
+            //openNewCellphoneDialog(uri);
+            /* */
+//            uri = data.getData();
+//            Bitmap bitmap = null;
+//            try {
+//                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            openNewCellphoneDialog(bitmap);
+            //Toast.makeText(this, path, Toast.LENGTH_SHORT).show();
         }
         else if (resultCode == RESULT_CANCELED) {
             Toast.makeText(this, "Picture was not taken", Toast.LENGTH_SHORT);
         }
     }
 
-    private void getPhotoDetails(String filepath) throws IOException {
-        ExifInterface exif = new ExifInterface(filepath);
-        String teste = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
-//        String teste2 = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
-//        String teste3 = exif.getAttribute(ExifInterface.TAG_GPS_TIMESTAMP);
-
-        Toast.makeText(this, teste, Toast.LENGTH_SHORT).show();
-    }
-
-    private void openNewCellphoneDialog(Uri uri) {
-        PostDialog dialog = new PostDialog(uri);
+    private void openNewCellphoneDialog(Bitmap bitmap) {
+        PostDialog dialog = new PostDialog(bitmap);
         dialog.show(getSupportFragmentManager(), "New Cellphone");
     }
 
+//    private Location getLocation(){
+//        FusedLocationProviderClient
+//    }
     //@Override
     public void persistNewCellphoneData(Cellphone cellphone, CellPhoneOpenHelper helper) {
         // Adding the values from the dialog listener to the database
--- app/src/main/java/com/pedromihael/desmascarandoapp/PostDialog.java
+++ app/src/main/java/com/pedromihael/desmascarandoapp/PostDialog.java
@@ -7,8 +7,16 @@ package com.pedromihael.desmascarandoapp;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.media.ExifInterface;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +27,10 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -35,7 +47,11 @@ public class PostDialog extends AppCompatDialogFragment {
     private DialogListener listener;
     private Uri uri;
     private ImageView img;
+    private Bitmap bitmap;
 
+    public PostDialog(Bitmap bitmap){
+        this.bitmap = bitmap;
+    }
     public PostDialog(Uri uri){
         this.uri = uri;
     }
@@ -64,7 +80,17 @@ public class PostDialog extends AppCompatDialogFragment {
 
         //Toast.makeText(getActivity(), mEditTextBrand.getText(), Toast.LENGTH_LONG).show();
         img = view.findViewById(R.id.imagem_da_galeria);
-        img.setImageURI(uri);
+        img.setImageBitmap(resize(bitmap, 960,1280));
+        String path = saveImage(bitmap);
+//        Toast.makeText(getActivity(), path, Toast.LENGTH_SHORT).show();
+//        try {
+//            getPhotoDetails(path);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        //Toast.makeText(getActivity(), "Image Saved!", Toast.LENGTH_SHORT).show();
+
         /**/
 
         mCancelButton.setOnClickListener((v) -> this.dismiss());
@@ -89,13 +115,57 @@ public class PostDialog extends AppCompatDialogFragment {
             "Para cadastrar um dispositivo, insira todos os campos. Para cadastrar uma marca, insira apenas a marca",
                 Toast.LENGTH_SHORT).show();
             } */
-
+            getDialog().dismiss();
         });
 
         mEditTextBrand = view.findViewById(R.id.edit_brand);
         mEditTextModel = view.findViewById(R.id.edit_model);
 
         return builder.create();
+    }
+
+    private Bitmap resize(Bitmap bitmap, int newWidth, int newHeight) {
+        Bitmap output = Bitmap.createBitmap(newWidth, newHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        Matrix m = new Matrix();
+        m.setScale((float) newWidth / bitmap.getWidth(), (float) newHeight / bitmap.getHeight());
+        canvas.drawBitmap(bitmap, m, new Paint());
+        return output;
+    }
+
+    private void getPhotoDetails(String filepath) throws IOException {
+        ExifInterface exif = new ExifInterface(filepath);
+        String teste = exif.getAttribute(ExifInterface.TAG_IMAGE_LENGTH);
+//        String teste2 = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
+//        String teste3 = exif.getAttribute(ExifInterface.TAG_GPS_TIMESTAMP);
+
+        Toast.makeText(getActivity(), teste, Toast.LENGTH_LONG).show();
+    }
+
+    private String saveImage(Bitmap bitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File directory = new File(
+                Environment.getExternalStorageDirectory() + "/Desmascarados_images");
+        // Criando o diretório caso ele não exista!
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+        try {
+            File fileImage = new File(directory, Calendar.getInstance().getTimeInMillis() + ".jpg");
+            fileImage.createNewFile();
+            FileOutputStream fo = new FileOutputStream(fileImage);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(getActivity(),
+                    new String[]{fileImage.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved:->" + fileImage.getAbsolutePath());
+            return fileImage.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
     }
 
     @Override
